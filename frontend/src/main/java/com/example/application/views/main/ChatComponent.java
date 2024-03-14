6e8a5676dca075c4daa134aae2230838614a3f4c
Mikel Perez <lekimperez@gmail.com>
2024-03-14 19:32:27 +0100

    The visualization of the chat is almost what I want

--- frontend/src/main/java/com/example/application/views/main/ChatComponent.java
+++ frontend/src/main/java/com/example/application/views/main/ChatComponent.java
@@ -5,7 +5,9 @@ import com.vaadin.flow.component.KeyNotifier;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Horizontal;
 import com.vaadin.flow.component.messages.MessageInput;
 import com.vaadin.flow.component.messages.MessageList;
 import com.vaadin.flow.component.messages.MessageListItem;
@@ -21,13 +23,29 @@ public class ChatComponent extends VerticalLayout {
     private Button sendButton;
 
     public ChatComponent() {
+        //Cuadro para los chats
         list = new MessageList();
+        list.setWidthFull();
+        list.setHeightFull();
+        //Textfield para el mensaje del usuario
         input = new TextField();
+
+        //Botón para enviar el mensaje
         sendButton = new Button("Enviar", VaadinIcon.PAPERPLANE.create());
-
         sendButton.addClickListener(clickEvent -> sendMessage(input.getValue()));
+        
+        //Layout para el textfield y su botón
+        HorizontalLayout chatLayout = new HorizontalLayout();
+        chatLayout.setWidthFull();
+        chatLayout.setFlexGrow(1, input);
+        chatLayout.setFlexGrow(0, sendButton);
+        chatLayout.add(input, sendButton);
 
-        add(list, input, sendButton);
+        //Layout de margen inferior
+        HorizontalLayout bottomMargin = new HorizontalLayout();
+        bottomMargin.setHeight("5%");
+
+        add(list, chatLayout, bottomMargin);
     }
 
     private void sendMessage(String message) {

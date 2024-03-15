b4044f7bc035d6ee6e77278a57d58ba328655789
mikel-p-h <lekimperez@gmail.com>
2024-03-15 18:28:05 +0100

    Everything working with ChatGPT API

---
+++ backend/src/test/java/com/hpe/boltech/BoltechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpe.boltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoltechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
